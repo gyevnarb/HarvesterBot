c0b0fd71a6c6390078207abdfe5a54f180ab258b
Murto <murto@airmail.cc>
2017-03-18 15:56:29 +0000

    Add bot class method stubs

--- TexasHoldEm/Bot.cs
+++ TexasHoldEm/Bot.cs
@@ -4,9 +4,36 @@
 /// Summary description for Class1
 /// </summary>
 public class Bot {
+	
+	private const int chromosomeSize = 10;
+	private Chromosome<double> chromosome;
+
 	public Bot() {
-		//
-		// TODO: Add constructor logic here
-		//
+		chromosome = new Chromosome<double>(chromosomeSize);
 	}
+
+	public Bot(double[] values) {
+		chromosome = new Chromosome<double>(values);
+	}
+
+	public bool betOrFold(int currentBet) {
+		// True is bet, False is fold
+		return true;
+	}
+
+	public int makeBet() {
+		// returns the ammount to bet
+		return 0;
+	}
+
+	private double getEnemyWinProb() {
+		// Returns the probability of the enemy winning
+		return 0.0;
+	}
+
+	private double getSelfWinProb() {
+		// Returns the probability of us winning
+		return 0.0;
+	}
+
 }
