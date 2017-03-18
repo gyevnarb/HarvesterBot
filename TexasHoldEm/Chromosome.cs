4059c8e9a0956d0b85773f8beddcaa21602713ae
Murto <murto@airmail.cc>
2017-03-18 15:17:27 +0000

    Add generic definiton

--- TexasHoldEm/Chromosome.cs
+++ TexasHoldEm/Chromosome.cs
@@ -3,8 +3,9 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Chromosome {
-	public Chromosome() {
+public class Chromosome<Type, Size> {
+
+    public Chromosome() {
 		//
 		// TODO: Add constructor logic here
 		//
