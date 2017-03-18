94e9995a62d0aa78284a4fe446ee7d086f8afb7d
Sinan Vanli <baldr@Baldr.local>
2017-03-18 20:53:24 +0000

    Cards?

--- TexasHoldEm/GameStateWrapper.cs
+++ TexasHoldEm/GameStateWrapper.cs
@@ -8,4 +8,20 @@ using AIGaming.Core.Games;
 public static class GameStateWrapper
 {
 	public static TexasHoldEmGameState THGS;
+
+	public static Card ourCard(int x) {
+		return THGS.playerHand[x-1];
+	}
+	public static Card tableCard(int x) {
+		if (x == 1)
+			return THGS.BoardCards.FirstOrDefault();
+		if (x == 2)
+			return THGS.BoardCards.SecondOrDefault();
+		if (x == 3)
+			return THGS.BoardCards.ThirdOrDefault();
+		if (x == 4)
+			return THGS.BoardCards.FourthOrDefault();
+		if (x == 5)
+			return THGS.BoardCards.FifthOrDefault();
+	}
 }
