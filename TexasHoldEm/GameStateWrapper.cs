b2fa3e7c4e5540c4c01fef531f4ac413133350ea
FireCube- <gyevnarb@gmail.com>
2017-03-18 21:00:27 +0000

    GameStateWrapper updated

--- TexasHoldEm/GameStateWrapper.cs
+++ TexasHoldEm/GameStateWrapper.cs
@@ -4,24 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AIGaming.Core.Games;
+using AIGaming.Core.Games.Helpers;
 
 public static class GameStateWrapper
 {
 	public static TexasHoldEmGameState THGS;
 
 	public static Card ourCard(int x) {
-		return THGS.playerHand[x-1];
+		return THGS.PlayerHand[x-1];
 	}
 	public static Card tableCard(int x) {
-		if (x == 1)
-			return THGS.BoardCards.FirstOrDefault();
-		if (x == 2)
-			return THGS.BoardCards.SecondOrDefault();
-		if (x == 3)
-			return THGS.BoardCards.ThirdOrDefault();
-		if (x == 4)
-			return THGS.BoardCards.FourthOrDefault();
-		if (x == 5)
-			return THGS.BoardCards.FifthOrDefault();
+        switch (x)
+        {
+            case 1:  return THGS.BoardCards.FirstOrDefault();
+            case 2:  return THGS.BoardCards.SecondOrDefault();
+            case 3:  return THGS.BoardCards.ThirdOrDefault();
+            case 4:  return THGS.BoardCards.FourthOrDefault();
+            case 5:  return THGS.BoardCards.FifthOrDefault();
+            default: return null;
+        }
 	}
 }
