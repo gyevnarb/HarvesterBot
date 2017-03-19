80d59f682e909f8dc77e85ad2119d8aa722529c4
Murto <murto@airmail.cc>
2017-03-19 11:52:38 +0000

    Remove comment

--- TexasHoldEm/Trainer.cs
+++ TexasHoldEm/Trainer.cs
@@ -51,7 +51,7 @@ public class Trainer {
 	public void nextGeneration() {
 		LogWriter.writeData(Environment.NewLine + "\t<GENERATING NEW POPULATION>" + Environment.NewLine + Environment.NewLine);
 		pop.Sort(geneSort);
-		//GeneReadWriter.writeGene("fittest.gene", pop[0]);
+		GeneReadWriter.writeGene("fittest.gene", pop[0]);
 		LogWriter.writeData(">Generation " + generation++ + ":" + Environment.NewLine);
 		writeGenes();
 		List<Gene> newPop = new List<Gene>(popSize);
