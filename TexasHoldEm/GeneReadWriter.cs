9ea8794d7e5c136903868b67e2a7f1e9f69fcd6a
Murto <murto@airmail.cc>
2017-03-19 10:38:38 +0000

    Add read-writer

---
+++ TexasHoldEm/GeneReadWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public static class GeneReadWriter {
+
+	public static Gene readGene(string path) {
+		using (Stream stream = File.Open(path, FileMode.Open)) {
+			var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+			return (Gene) binaryFormatter.Deserialize(stream);
+		}
+	}
+
+	public static void writeGene(string path, Gene gene) {
+		using (Stream stream = File.Open(path, FileMode.Create)) {
+			var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+			binaryFormatter.Serialize(stream, gene);
+		}
+	}	
+}
